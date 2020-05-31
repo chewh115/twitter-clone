c1787d6198220999615b550623b9bb93c2e6924d
Chelsea White <chewh115@gmail.com>
2020-05-31 10:42:13 -0400

    Sets up tweet model

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from twitteruser.models import TwitterUser
 
 # Create your models here.
+class Tweet(models.Model):
+    tweet = models.CharField(max_length=140)
+    time_tweeted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        TwitterUser,
+        on_delete=models.CASCADE,
+        related_name='tweet_author'
+        )
+    
+    def __str__(self):
+        return self.tweet
