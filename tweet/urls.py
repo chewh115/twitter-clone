bdc78402dc3b27e5847dce5738224f32b691c874
Chelsea White <chewh115@gmail.com>
2020-05-31 12:27:16 -0400

    Creating new tweet working

--- tweet/urls.py
+++ tweet/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.tweetindex, "tweethome")
+    path('tweet/', views.createtweet, name="createtweet")
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from .forms import TweetForm
+from .models import Tweet
+from datetime import datetime
 
 # Create your views here.
-def tweetindex(request):
-    return render(request, 'tweet/index.html')
+def createtweet(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            new_tweet = form.cleaned_data
+            Tweet.objects.create(
+                tweet = new_tweet['tweet'],
+                time_tweeted = datetime.now(),
+                author = request.user
+            )
+            return HttpResponseRedirect(reverse('home'))
+    form = TweetForm()
+    return render(request, 'createtweet.html', {'form': form})
