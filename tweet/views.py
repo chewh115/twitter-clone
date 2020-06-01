e54e505946afc4c0982554b2452d56c678dc3e73
Chelsea White <chewh115@gmail.com>
2020-06-01 15:10:21 -0400

    Updates so createtweet view is only accessible by logged in user

--- tweet/views.py
+++ tweet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.shortcuts import render, reverse, HttpResponseRedirect, redirect
 from .forms import TweetForm
 from .models import Tweet
 from datetime import datetime
@@ -8,27 +8,29 @@ import re
 
 # Create your views here.
 def createtweet(request):
-    if request.method == "POST":
-        form = TweetForm(request.POST)
-        if form.is_valid():
-            tweet_data = form.cleaned_data
-            new_tweet = Tweet.objects.create(
-                tweet = tweet_data['tweet'],
-                time_tweeted = datetime.now(),
-                author = request.user
-            )
-            new_tweet.save()
-            to_notify = re.findall("\@(\w*)\b", tweet_data['tweet'])
-            for user in to_notify:
-                new_notif = Notification.objects.create(
-                    tweet = Tweet.objects.get(id=new_tweet.id),
-                    notified_user = TwitterUser.objects.get(username=user),
-                    viewed=False
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = TweetForm(request.POST)
+            if form.is_valid():
+                tweet_data = form.cleaned_data
+                new_tweet = Tweet.objects.create(
+                    tweet = tweet_data['tweet'],
+                    time_tweeted = datetime.now(),
+                    author = request.user
                 )
-                new_notif.save()
-            return HttpResponseRedirect(reverse('home'))
-    form = TweetForm()
-    return render(request, 'createtweet.html', {'form': form})
+                new_tweet.save()
+                to_notify = re.findall("\@(\w*)\b", tweet_data['tweet'])
+                for user in to_notify:
+                    new_notif = Notification.objects.create(
+                        tweet = Tweet.objects.get(id=new_tweet.id),
+                        notified_user = TwitterUser.objects.get(username=user),
+                        viewed=False
+                    )
+                    new_notif.save()
+                return HttpResponseRedirect(reverse('home'))
+        form = TweetForm()
+        return render(request, 'createtweet.html', {'form': form})
+    return redirect('/login/')
 
 
 def tweetdetail(request, id):
