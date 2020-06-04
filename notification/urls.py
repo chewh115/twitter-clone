e2d5a50092fba1ab0875a8254738ca5a3aaa7ca1
Chelsea White <chewh115@gmail.com>
2020-06-04 12:02:33 -0400

    Has notifications working!

--- notification/urls.py
+++ notification/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from notification import views
 
 urlpatterns = [
-    path('notifications/', views.notifications, name="notifications")
+    path('notifications/', views.notifs, name="notifs")
 ]
--- notification/views.py
+++ notification/views.py
@@ -5,15 +5,15 @@ from twitteruser.models import TwitterUser
 
 
 # Create your views here.
-def notifications(request):
-    if request.user.is_authenticated:
-        notifications = Notification.objects.filter(notified_user=request.user, viewed=False)
-        # notifs = []
-        if notifications:
-            for notification in notifications:
-                # notifs.append(notification)
-                notification.viewed = True
-                notification.save()
-            user = TwitterUser.objects.get(username=request.user.username)
-            return render(request, 'notifications.html', {'notifications': notifications})
-    return redirect('/login/')
+def notifs(request):
+    notifications = Notification.objects.filter(notified_user=request.user, viewed=False)
+    if notifications:
+        for notification in notifications:
+            notification.viewed = True
+            notification.save()
+        user = TwitterUser.objects.get(username=request.user.username)
+        return render(
+            request,
+            'notifications.html',
+            {'notifications': notifications, 'user': user})
+    return render(request, 'notifications.html')
